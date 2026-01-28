acb63bab414f7cdb68448bb168b2a2475dbd7d85
Orpita <dasorpita100@gmail.com>
2026-01-28 18:18:23 +0530

    Day 3: Added one new DSA program

---
+++ palindrome2pointer_3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number of elements: "<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"enter elements: "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bool f= true;
+    int j=n-1;
+    int i=0;
+    while(i<j){
+        if(arr[j]!=arr[i]){
+            f= false;
+            break;
+        }
+        j--;
+        i++;
+    }
+    if(f){
+        cout<<"palindrome"<<endl;
+    }
+    else{
+        cout<<"not palindrome"<<endl;
+    }
+    return 0;
+}
