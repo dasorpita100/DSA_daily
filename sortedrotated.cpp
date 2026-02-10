6497b03ce7f3c7182928b323f80ee370cda267ca
Orpita <dasorpita100@gmail.com>
2026-02-10 17:48:09 +0530

    check if array is sorted and rotated

---
+++ sortedrotated.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number of elements: "<<endl;
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"enter elements: "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int drop=0;
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            drop++;
+        }
+    }
+    if(arr[n-1]>arr[0]){
+        drop++;
+    }
+    if(drop<=1) {
+        cout<<"sorted and rotated"<<endl;
+    }
+    else{
+        cout<<"not sorted and not rotated"<<endl;
+    }
+}
