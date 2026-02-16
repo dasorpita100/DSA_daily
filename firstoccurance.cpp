dc7dab50c08739d19224fdcbe90991d4331662f4
Orpita <dasorpita100@gmail.com>
2026-02-16 20:44:00 +0530

    first and last occurance of element in binary search

---
+++ firstoccurance.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number of elements: "<<endl;
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"enter elements: "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cout<<"enter the target: "<<endl;
+    cin>>target;
+    int low= 0, high= n-1;
+    int pos=-1;
+    while(low<=high){
+        int mid= low+(high-low)/2;
+        if(arr[mid]==target){
+            pos= mid;
+            high= mid-1;
+        }
+        else if(arr[mid]>target){
+            high= mid-1;
+        }
+        else{
+            low= mid+1;
+        }
+    }
+    cout<<"the first occurance is: "<<pos<<endl;
+}
